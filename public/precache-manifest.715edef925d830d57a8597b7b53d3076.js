a48995f4d551d255677f1929a3d047675acb3f03
nicoAlvarezMern <nicolas.alvarez.mern@gmail.com>
2020-09-20 11:23:10 -0300

    1.2

---
+++ public/precache-manifest.715edef925d830d57a8597b7b53d3076.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7460d66e55351ba5c54f6fb2d0c6d98d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fff69f44fcd92b2edf41",
+    "url": "/static/css/2.dabbbe1b.chunk.css"
+  },
+  {
+    "revision": "8c44e81705fc1bfcb56d",
+    "url": "/static/css/main.a0c100cc.chunk.css"
+  },
+  {
+    "revision": "fff69f44fcd92b2edf41",
+    "url": "/static/js/2.fcf08018.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.fcf08018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c44e81705fc1bfcb56d",
+    "url": "/static/js/main.e54eb886.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
